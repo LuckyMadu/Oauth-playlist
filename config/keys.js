e9b09d9e3c20bd6910ccd05698039f913b46fbcc
LuckyMadu <lahiruamarathunge94@gmail.com>
2018-08-13 14:43:35 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,7 @@
 module.exports = {
   google: {
     clientID:
-      "388247221-0m07c5umdm3l073p54snpv6jl151s49n.apps.googleusercontent.com",
-    clientSecret: "UMjnqISfb8l3jn78GiF-ju_j"
+      "", //your id
+    clientSecret: "" //your secret
   }
 };

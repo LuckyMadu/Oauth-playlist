856a5d5155678233b620dcb70bab69f0268a43a9
lahiru <lahiruamarathunga526@gmail.com>
2018-08-13 14:42:30 +0530

    added keys file

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//add this file to .gitignore
+//author
+module.exports = {
+  google: {
+    clientID:
+      "388247221-0m07c5umdm3l073p54snpv6jl151s49n.apps.googleusercontent.com",
+    clientSecret: "UMjnqISfb8l3jn78GiF-ju_j"
+  }
+};
